630749cce865f1a31304e149535c7e37373163f2
Radosław <radek.barylak@gmail.com>
2025-11-29 18:53:17 +0100

    Added events handling

--- backend/apes_api_strong/apes_api_strong/urls.py
+++ backend/apes_api_strong/apes_api_strong/urls.py
@@ -43,6 +43,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('api/', include('events.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
