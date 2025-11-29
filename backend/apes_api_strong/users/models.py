15eedf7848ba11e31e311b98c6bd9bb9bee9b54a
Radosław <radek.barylak@gmail.com>
2025-11-29 12:56:14 +0100

    Initial commit. Swagger setup. JWT setup.

---
+++ backend/apes_api_strong/users/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    bananas = models.PositiveIntegerField(default=0)
+
+    PERSONALITY_CHOICES = [
+        ("INTJ", "Architect (INTJ)"),
+        ("INTP", "Logician (INTP)"),
+        ("ENTJ", "Commander (ENTJ)"),
+        ("ENTP", "Debater (ENTP)"),
+        ("INFJ", "Advocate (INFJ)"),
+        ("INFP", "Mediator (INFP)"),
+        ("ENFJ", "Protagonist (ENFJ)"),
+        ("ENFP", "Campaigner (ENFP)"),
+        ("ISTJ", "Logistician (ISTJ)"),
+        ("ISFJ", "Defender (ISFJ)"),
+        ("ESTJ", "Executive (ESTJ)"),
+        ("ESFJ", "Consul (ESFJ)"),
+        ("ISTP", "Virtuoso (ISTP)"),
+        ("ISFP", "Adventurer (ISFP)"),
+        ("ESTP", "Entrepreneur (ESTP)"),
+        ("ESFP", "Entertainer (ESFP)"),
+    ]
+
+    personality = models.CharField(
+        max_length=4,
+        choices=PERSONALITY_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.username
+    
+
+
+    
+    
