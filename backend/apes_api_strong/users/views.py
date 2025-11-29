22fc0d3bc80fcab6b80cae7a853f6b77bfc56a0b
Radosław <radek.barylak@gmail.com>
2025-11-29 16:54:44 +0100

    Quick import fix

--- backend/apes_api_strong/users/views.py
+++ backend/apes_api_strong/users/views.py
@@ -1,4 +1,4 @@
-from requests import Response
+from rest_framework.response import Response
 from apes_api_strong import settings
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
