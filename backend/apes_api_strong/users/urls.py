7d675ba11de8c1b28f4b98ddd9061dadecbdc6fe
Radosław <radek.barylak@gmail.com>
2025-11-29 16:00:47 +0100

    Added tags and openai sugesstions

--- backend/apes_api_strong/users/urls.py
+++ backend/apes_api_strong/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet)  # puste = /api/users/ jako root
+router.register('tags', TagViewSet)  # puste = /api/tags/ jako root
 
 urlpatterns = router.urls
